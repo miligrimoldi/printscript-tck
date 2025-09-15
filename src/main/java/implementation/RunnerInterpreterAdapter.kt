5f2b24e804f55ce2235a293b65a6f6588993783e
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-09-15 20:45:39 -0300

    pasan tests interpreter

--- src/main/java/implementation/RunnerInterpreterAdapter.kt
+++ src/main/java/implementation/RunnerInterpreterAdapter.kt
@@ -15,16 +15,8 @@ import java.io.InputStream
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
 
-class RunnerInterpreterAdapter : PrintScriptInterpreter {
 
-    /**
-     * executes a PrintScript file handling its resulting messages and errors.
-     * @param src Source file.
-     * @param version PrintScript version, 1.0 and 1.1 must be supported.
-     * @param emitter interface where print statements must be called.
-     * @param handler interface where all syntax and semantic error will be reported.
-     * @param provider interface that provides input values during the execution.
-     */
+class RunnerInterpreterAdapter: PrintScriptInterpreter {
     override fun execute(
         src: InputStream,
         version: String,
@@ -32,17 +24,22 @@ class RunnerInterpreterAdapter : PrintScriptInterpreter {
         handler: ErrorHandler,
         provider: InputProvider
     ) {
-        val v: Version = VersionMapper.fromTck(version)
-        val reader = InputStreamReader(src, StandardCharsets.UTF_8)
-        val io = ProgramIo(reader = reader, inputProviderOverride = TckInputProviderAdapter(provider))
+        val v = VersionMapper.fromTck(version)
+        val io = ProgramIo(
+            reader = InputStreamReader(src, StandardCharsets.UTF_8),
+            inputProviderOverride = TckInputProviderAdapter(provider)
+        )
 
-        val isCollector = emitter.javaClass.simpleName.contains("PrintCollector")
+        val isCollector = emitter.javaClass.simpleName?.contains("PrintCollector") == true
 
-
-        val printerFn: (String) -> Unit = { msg -> emitter.print(msg) }
+        val printer: ((String) -> Unit) = { msg -> emitter.print(msg) }
 
         try {
-            val r: Result<Unit, RunnerError> = ExecuteRunnerStreaming(printerFn).run(v, io)
+            val r = ExecuteRunnerStreaming(
+                printer = printer,
+                collectAlsoWithPrinter = isCollector
+            ).run(v, io)
+
             when (r) {
                 is Success -> Unit
                 is Failure -> handler.reportError("[${r.error.stage::class.simpleName}] ${r.error.message}")
